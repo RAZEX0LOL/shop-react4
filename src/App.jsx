012ab2a39cad7868b9251c9b9de5bde38a0e4625
RAZEX0LOL <99183060+RAZEX0LOL@users.noreply.github.com>
2025-02-28 20:56:55 +0400

    add pages in project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import './App.scss'
+import { useState } from 'react'
+import { Route, Router, Routes } from 'react-router-dom'
+import './App.css'
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import CartPage from './pages/CartPage'
+import Home from './pages/Home'
+import ProductDetail from './pages/ProductDetail'
 
 export default function App() {
+  const [cartItems, setCartItems] = useState([])
+
+  const addToCart = (product) => {
+    setCartItems(prev => [...prev, product])
+  }
+
+  const removeFromCart = (id) => {
+    setCartItems(prev => prev.filter(item => item.id !== id))
+  }
 
   return (
-    <>
-    </>
+    <Router>
+      <Header />
+      <main className='min-h-screen'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetail addToCart={addToCart} />} />
+          <Route path="/cart" element={<CartPage cartItems={cartItems} removeFromCart={removeFromCart} />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   )
 }
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 
+// eslint-disable-next-line react/prop-types
 const ProductCard = ({ product }) => {
 	return (
 		<div className='bg-white rounded shadow p-4'>
